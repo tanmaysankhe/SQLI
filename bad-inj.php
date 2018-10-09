cc71951671097f138c04fcb98d34c6aad694a87a
tanmaysankhe <tanmaysankhe97@gmail.com>
2018-10-09 21:10:49 +0530

    GUI added and few more modifications

--- bad-inj.php
+++ bad-inj.php
@@ -1,18 +1,39 @@
 <?php require_once 'header.php' ?>
 
-<p align="center">Welcome To Unsafe Bank</p>
 
-<form action="safeLogin.php" method="post">
-<p align="center">
-Email id: <input type="text" name="email" size="40">  
-</p>
+<head>
+	<link rel="stylesheet" type="text/css" href="w3css.css"></link>
+</head>
+	<body style="background-image: url('backg.jpg'); height: 100%;
 
-<p align="center">
-Password:      <input type="password" name="password" size="40">  
-<br> 
-<input align="center" type="submit" value="Submit">
-</p>
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;">
+	<p align="center" style="font-size: 70px; color: white;">Welcome To The Book Bank</p>
+<div class=" w3-container w3-center" style="margin-left: 30%" >
+ <div class="w3-card-4 w3-white" style="width: 50%">
+  <img src="banner-books.png" alt="Norway" style="width: 100%">
+  <!-- <div class="w3-container w3-center"> -->
+    <div class="w3-card w3-white">
+		<form action="safeLogin.php" method="post">
+		<p align="center">
+		<input class="w3-button w3-white w3-border w3-border-red w3-round-large" type="text" name="email" size="40" placeholder="Email">  
+		</p>
+
+		<p align="center">
+		<input class="w3-button w3-white w3-border w3-border-red w3-round-large" type="text" name="password" size="40" placeholder="password">  
+		
+		<br><br> 
+		<input class="w3-button w3-white w3-border w3-border-blue w3-round-large" align="center" type="submit" value="Submit">
+		</p>
+		<br>
+	</form>
+</div>
+  <!-- </div> -->
+</div> 
+</div>
 
 
-    
-</form>
+
+</body>
--- safeLogin.php
+++ safeLogin.php
@@ -1,27 +1,48 @@
 <?php require_once 'header.php' ?>
 
-<p>This is Unsafe Bank 2 and you just logged in:<br>
     
+<head>
+    <link rel="stylesheet" type="text/css" href="w3css.css"></link>
+</head>
+    <body style="background-color: grey">
+
+         <div class="w3-bar w3-border w3-grey">
+  <a href="#" class="w3-bar-item w3-button">Home</a>
+  <a href="#" class="w3-bar-item w3-button">Buy</a>
+  <!-- <a href="#" class="w3-bar-item w3-button">Link 2</a> -->
+  <a href="bad-inj.php" class="w3-bar-item w3-button w3-dark-grey w3-right">Logout</a>
+</div> 
+ <table class="w3-table-all w3-hoverable" style="margin: 10px"> 
     <?php
     
     $username = $_POST['email'];
     $password = $_POST['password'];
 
-    $con = mysqli_connect('127.0.0.1', 'root', '', 'hello');
+    $con = mysqli_connect('127.0.0.1', 'root', '', 'my_dbname');
 
-    $query = sprintf("SELECT * FROM my_users WHERE user_email = '%s' AND user_pass = '%s';",
-                     mysqli_real_escape_string($con, $username),
+    $query = sprintf("SELECT * FROM my_users WHERE user_email = '%s' AND (user_pass = '%s');",
+                      mysqli_real_escape_string($con, $username),
                      mysqli_real_escape_string($con, $password));
-//    $query = sprintf("SELECT * FROM my_users");
+                //    $username,
+              //   $password);
+    // $query = "SELECT * FROM my_users WHERE user_email =". $username ."  AND user_pass = ".$password .";;";
     $result = mysqli_query($con, $query);
+    echo "<table class='w3-table-all w3-hoverable' style='margin: 10px'>";
+    if ($result-> num_rows > 0){
+        foreach ($result as $row) {
+       echo "<tr><th>Name</th><td>".$row['display_name']."</td></tr>";
+       echo "<tr><th>Email</th><td>".$row['user_email']."</td></tr>";
+       echo "<tr><th>Books owned</th><td>".$row['num_books']."</td></tr>";
+       echo "<tr><th>Contact</th><td>".$row['contact']."</td></tr>";
+       echo "<tr><th>Balance($)</th><td>".$row['bank_bal']."</td></tr>";
 
-    if ($result->num_rows > 0){
-    echo "Authenticated as " . $username;
+        }
+    echo "</table>";
     }
+    
     else{
     echo "Wrong username/password combination.";
     }
     
     ?>
-    
-</p>    
+</body>
